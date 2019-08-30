26db3efec7fce3a6a314a6b94b8c7ef2da869704
zhaoxiang <xiang.zhao@ucdconnect.ie>
2019-08-30 13:33:33 +0100

    problem 2 - check permutation

---
+++ 1_Array_and_Strings/check_permutation.cpp
@@ -0,0 +1,27 @@
+#include <cstddef>
+#include <cstring>
+
+bool checkPermutation(const char *str1, const char *str2) {
+    size_t len1 = strlen(str1);
+    size_t len2 = strlen(str2);
+
+    if (len1 != len2)
+        return false;
+
+    bool table[52]{false};
+
+    for (size_t index = 0; index < len1; index++) {
+        table[str1[index] - 'A'] = true;
+    }
+
+    for (size_t index = 0; index < len2; index++) {
+        table[str2[index] - 'A'] = false;
+    }
+
+    for (bool index : table) {
+        if (index)
+            return false;
+    }
+
+    return true;
+}
--- 1_Array_and_Strings/main.cpp
+++ 1_Array_and_Strings/main.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 extern bool is_unique(const char *str);
+extern bool checkPermutation(const char *str1, const char *str2);
 
 int main() {
-    bool result = is_unique("hello");
-    if (result)
-        cout << "is unique: true" << endl;
+    bool result = checkPermutation("hello", "olleh");
 
+    if (result)
+        cout << "check permutation: true" << endl;
     return 0;
 }
