c79f2e1ecd57a5a0482416825e53a7c6e94d7bc2
zhaoxiang <xiang.zhao@ucdconnect.ie>
2019-08-30 14:18:05 +0100

    improve compile speed

--- 1_Array_and_Strings/test.cpp
+++ 1_Array_and_Strings/test.cpp
@@ -1,13 +1,14 @@
-#define CATCH_CONFIG_MAIN
-
 #include <catch2/catch.hpp>
 
 extern bool isUnique(const char *str);
 extern bool checkPermutation(const char *str1, const char *str2);
 
-TEST_CASE("Check Permutation") {
+TEST_CASE("Is Unique") {
     REQUIRE(!isUnique("hello"));
     REQUIRE(isUnique("abc"));
+}
+
+TEST_CASE("Check Permutation") {
     REQUIRE(checkPermutation("hello", "hello"));
     REQUIRE(!checkPermutation("abc", "abd"));
 }
