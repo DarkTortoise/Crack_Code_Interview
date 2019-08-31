6deb17d5f8b54059994a177c515a3daa89c12850
zhaoxiang <xiang.zhao@ucdconnect.ie>
2019-08-31 18:21:55 +0100

    problem 4 - parlindrome permutation

---
+++ 1_Array_and_Strings/parlindromePermutation.cpp
@@ -0,0 +1,29 @@
+#include <cstddef>
+#include <cstring>
+
+bool parPermutation(const char *str) {
+    size_t length = strlen(str);
+    int table[52]{0};
+
+    for (size_t index = 0; index < length; index++) {
+        size_t pos = str[index] - 'A';
+        table[pos]++;
+    }
+
+    bool onlyOneCount = false;
+
+    for (int count : table) {
+        if (count % 2 != 0) {
+            if (length % 2 == 0)
+                return false;
+            else {
+                if (onlyOneCount)
+                    return false;
+                else
+                    onlyOneCount = true;
+            }
+        }
+    }
+
+    return true;
+}
--- 1_Array_and_Strings/test.cpp
+++ 1_Array_and_Strings/test.cpp
@@ -6,6 +6,8 @@ using namespace std;
 extern bool isUnique(const char *str);
 extern bool checkPermutation(const char *str1, const char *str2);
 extern string URLify(const char *str, size_t size);
+extern bool parPermutation(const char *str);
+extern bool parPermutationS2(const char *str);
 
 TEST_CASE("Is Unique") {
     REQUIRE(!isUnique("hello"));
@@ -20,4 +22,9 @@ TEST_CASE("Check Permutation") {
 TEST_CASE("URLIFY") {
     REQUIRE(URLify("Hello World", 11) == "Hello%20World");
     REQUIRE(URLify("Hello World ", 12) == "Hello%20World%20");
+}
+
+TEST_CASE("Parlindrome Permutation") {
+    REQUIRE(!parPermutation("tact coa"));
+    REQUIRE(parPermutation("abccba"));
 }
