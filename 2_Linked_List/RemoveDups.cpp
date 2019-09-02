3d76b63f535e02ada0efecb92bddddf66e1e5694
zhaoxiang <xiang.zhao@ucdconnect.ie>
2019-09-02 12:48:40 +0100

    Problem - Remove Dups

--- 2_Linked_List/RemoveDups.cpp
+++ 2_Linked_List/RemoveDups.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// Space complexity: O(N)
 list<int> &RemoveDups(list<int> &L) {
     set<int> keys{};
 
@@ -12,6 +13,23 @@ list<int> &RemoveDups(list<int> &L) {
             index++;
         } else {
             index = L.erase(index);
+        }
+    }
+
+    return L;
+}
+
+// Space complexity: O(1)
+list<int> &RemoveDupsV2(list<int> &L) {
+    auto index = begin(L);
+    auto ptr = begin(L);
+
+    while (index != end(L) && ptr != end(L)) {
+        if (index != ptr && *index == *ptr) {
+            ptr = L.erase(ptr);
+        } else {
+            index = ptr;
+            ptr++;
         }
     }
 
--- 2_Linked_List/test.cpp
+++ 2_Linked_List/test.cpp
@@ -5,10 +5,12 @@
 using namespace std;
 
 extern list<int> &RemoveDups(list<int> &L);
+extern list<int> &RemoveDupsV2(list<int> &L);
 
 TEST_CASE("Remove Dups") {
     list<int> L1{1, 1, 4, 4, 5};
     list<int> L2{1, 4, 5};
 
     REQUIRE(RemoveDups(L1) == L2);
+    REQUIRE(RemoveDupsV2(L1) == L2);
 }
