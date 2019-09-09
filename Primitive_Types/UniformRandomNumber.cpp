e3e6fec1fa765c10306c448a6da32e5f87fec29a
zhaoxiang <xiang.zhao@ucdconnect.ie>
2019-09-09 22:05:39 +0100

    Uniform Random Number

---
+++ Primitive_Types/UniformRandomNumber.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <ctime>
+
+int ZeroOneRandom() {
+    srand(time(nullptr));
+    int result = rand() % 100 + 1;
+
+    if (result % 2 == 0)
+        return 0;
+    else
+        return 1;
+}
+
+int UniformRandomNumber(int low, int high) {
+    int n = high - low + 1;
+    int nDigits = int(ceil(log2(n))) + 1;
+    int result = 0;
+
+    do {
+        result = 0;
+        for (int index = 0; index < nDigits; index++) {
+            int coin = ZeroOneRandom();
+            result += int(pow(2, index)) * coin;
+        }
+    } while (result > n);
+
+    return result + low;
+}
--- Primitive_Types/test.cpp
+++ Primitive_Types/test.cpp
@@ -6,6 +6,8 @@ extern long SameWeight(long x);
 extern long ReverseDigit(long x);
 extern bool PalindromeDecimal(long x);
 
+extern int UniformRandomNumber(int low, int high);
+
 TEST_CASE("Computer parity for a large number") {
     REQUIRE(Parity(64) == 1);
 }
@@ -29,4 +31,14 @@ TEST_CASE("Palindrome Decimal") {
     REQUIRE(PalindromeDecimal(121));
     REQUIRE(PalindromeDecimal(1221));
     REQUIRE(!PalindromeDecimal(123));
+}
+
+TEST_CASE("Generate Random Number") {
+    int n = UniformRandomNumber(1, 6);
+    REQUIRE(n >= 1);
+    REQUIRE(n <= 6);
+
+    n = UniformRandomNumber(10, 20);
+    REQUIRE(n >= 10);
+    REQUIRE(n <= 20);
 }
