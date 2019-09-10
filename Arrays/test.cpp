8b7927936882f5ef3f4bdf344eac54179ed29e27
zhaoxiang <xiang.zhao@ucdconnect.ie>
2019-09-10 22:07:53 +0100

    Array Partition

--- Arrays/test.cpp
+++ Arrays/test.cpp
@@ -1,1 +1,8 @@
 #include <catch2/catch.hpp>
+#include "ArrayPartition.h"
+
+TEST_CASE("Array Parition") {
+    vector<int> arr{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    vector<int> result{10, 2, 8, 4, 6, 7, 5, 9, 3, 1};
+    REQUIRE(ArrayPartition<int>(arr) == result);
+}
