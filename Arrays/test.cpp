e16e1937ab772f2f4fc877454d41245965aeb729
zhaoxiang <xiang.zhao@ucdconnect.ie>
2019-09-11 15:19:56 +0100

    Increment Arbitrary Precision Integer

--- Arrays/test.cpp
+++ Arrays/test.cpp
@@ -1,6 +1,11 @@
 #include <catch2/catch.hpp>
+#include <list>
 #include "ArrayPartition.h"
 #include "DutchNationalFlag.h"
+
+using namespace std;
+
+extern long IntegerIncrement(list<int> n);
 
 TEST_CASE("Array Parition") {
     vector<int> arr{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -17,4 +22,9 @@ TEST_CASE("Dutch National Flag Problem") {
     array<int, 10> a2{1, 2, 3, 1, 2, 3, 1, 2, 3, 1};
     array<int, 10> r1{1, 1, 1, 1, 2, 2, 2, 3, 3, 3};
     REQUIRE(DutchNationalFlagVariant1(a2, 1, 2, 3) == r1);
+}
+
+TEST_CASE("Increment Arbitrary Precision Integer") {
+    REQUIRE(IntegerIncrement(list<int>{1, 2, 9}) == 130);
+    REQUIRE(IntegerIncrement(list<int>{9, 9, 9}) == 1000);
 }
