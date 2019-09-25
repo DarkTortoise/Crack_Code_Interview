0f2510086755041a9b536973b44ea555e74b69ed
zhaoxiang <xiang.zhao@ucdconnect.ie>
2019-09-26 00:04:19 +0100

    Strings - Merge two lists

--- LinkedList/MergeTwoLists.h
+++ LinkedList/MergeTwoLists.h
@@ -7,21 +7,39 @@ using namespace std;
 
 template<class T>
 shared_ptr<ListNode<T>> MergeTwoLists(shared_ptr<ListNode<T>> r1, shared_ptr<ListNode<T>> r2) {
-    shared_ptr<ListNode<T>> root;
+    shared_ptr<ListNode<T>> root = nullptr;
+    shared_ptr<ListNode<T>> ptr = root;
 
     while (r1 != nullptr and r2 != nullptr) {
         if (r1->value < r2->value) {
-            root->next = r1;
-            root = root->next;
+            if (root == nullptr) {
+                root = r1;
+                ptr = root;
+            } else {
+                ptr->next = r1;
+                ptr = ptr->next;
+            }
+
+            r1 = r1->next;
+        } else {
+            if (root == nullptr) {
+                root = r2;
+                ptr = root;
+            } else {
+                ptr->next = r2;
+                ptr = ptr->next;
+            }
+
+            r2 = r2->next;
         }
     }
 
     if (r1 != nullptr) {
-        root->next = r1;
+        ptr->next = r1;
     }
 
     if (r2 != nullptr)
-        root->next = r2;
+        ptr->next = r2;
 
     return root;
 }
--- LinkedList/test.cpp
+++ LinkedList/test.cpp
@@ -13,3 +13,17 @@ TEST_CASE("Linked list") {
 
     REQUIRE((*r1).Equal(r1, r2));
 }
+
+TEST_CASE("Merge two lists") {
+    auto arr1 = array<int, 3>{2, 5, 7};
+    auto arr2 = array<int, 2>{3, 11};
+    auto arr3 = array<int, 5>{2, 3, 5, 7, 11};
+
+    auto r1 = ListNode<int>::CreateListFromArray(arr1);
+    auto r2 = ListNode<int>::CreateListFromArray(arr2);
+    auto r3 = ListNode<int>::CreateListFromArray(arr3);
+
+    auto r = MergeTwoLists(r1, r2);
+
+    REQUIRE((*r).Equal(r, r3));
+}
