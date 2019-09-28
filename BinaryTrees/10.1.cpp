08ae6595a7a5f8c10bd7828f49375b4561922bc3
zhaoxiang <xiang.zhao@ucdconnect.ie>
2019-09-28 23:07:34 +0100

    Binary Trees - 10.1 variant 2

--- BinaryTrees/10.1.cpp
+++ BinaryTrees/10.1.cpp
@@ -24,6 +24,37 @@ int PostTravel(const vector<int> &tree, size_t node, int height) {
     }
 }
 
+int Variant2(const vector<int> &tree, size_t node, int height, int k, int &theNode) {
+    if (node >= tree.size() or tree[node] == -1)
+        return height - 1;
+
+    auto left = node * 2 + 1;
+    auto right = node * 2 + 2;
+
+    auto leftDepth = PostTravel(tree, left, height + 1);
+    auto rightDepth = PostTravel(tree, right, height + 1);
+    auto diff = abs(long(leftDepth - rightDepth));
+
+    if (leftDepth == -1) {
+        theNode = left;
+        return -1;
+    } else if (rightDepth == -1) {
+        theNode = right;
+        return -1;
+    } else if (diff > k) {
+        theNode = node;
+        return -1;
+    } else {
+        return max(leftDepth, rightDepth);
+    }
+}
+
 bool Problem_10_1(const vector<int> &tree) {
     return PostTravel(tree, 0, 0) != -1;
+}
+
+int Problem_10_1_Variant_2(const vector<int> &tree, int k) {
+    int node = -1;
+    Variant2(tree, 0, 0, k, node);
+    return node;
 }
--- BinaryTrees/test.cpp
+++ BinaryTrees/test.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 extern bool Problem_10_1(const vector<int> &tree);
+extern int Problem_10_1_Variant_2(const vector<int> &tree, int k);
 
 TEST_CASE("test if a tree is height balanced") {
     const vector<int> tree{1, 2, 3, 4, -1, 5, -1, 6, -1};
@@ -12,4 +13,6 @@ TEST_CASE("test if a tree is height balanced") {
 
     const vector<int> t2{1, 2, 3, 4, 5};
     REQUIRE(Problem_10_1(t2));
+
+    REQUIRE(Problem_10_1_Variant_2(tree, 1) == 1);
 }
