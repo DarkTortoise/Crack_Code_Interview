4c3d5b82ea9581b93599f952468cf113c5a92275
zhaoxiang <xiang.zhao@ucdconnect.ie>
2019-09-30 21:17:16 +0100

    Binary Trees - 10.7

--- BinaryTrees/test.cpp
+++ BinaryTrees/test.cpp
@@ -9,6 +9,7 @@ extern int Problem_10_1_Variant_2(const vector<int> &tree, int k);
 extern size_t Problem_10_3(const vector<int> &t, size_t n1, size_t n2);
 extern int Problem_10_5(const vector<int> &t);
 extern bool Problem_10_2(const vector<int> &t);
+extern vector<int> Problem_10_7(const vector<int> &t);
 
 TEST_CASE("test if a tree is height balanced") {
     const vector<int> tree{1, 2, 3, 4, -1, 5, -1, 6, -1};
@@ -38,4 +39,10 @@ TEST_CASE("compute the lowest common ancestor in a binary tree") {
 TEST_CASE("sum the root-to-leaf in a binary tree") {
     const vector<int> t1{1, 0, 0, 1, 0, -1, -1, -1, -1, 1, -1};
     REQUIRE(Problem_10_5(t1) == 16);
+}
+
+TEST_CASE("implement an inorder traversal without recursion") {
+    const vector<int> t1{1, 2, 3, 4, 5};
+    const vector<int> r{4, 2, 5, 1, 3};
+    REQUIRE(Problem_10_7(t1) == r);
 }
