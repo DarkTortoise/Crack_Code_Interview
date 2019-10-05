17952754a00d2ea2a797e9ba36d018c90557769d
zhaoxiang <xiang.zhao@ucdconnect.ie>
2019-10-05 16:02:22 +0100

    Searching - 12.1

--- Searching/12.1.cpp
+++ Searching/12.1.cpp
@@ -9,3 +9,23 @@ size_t Problem_12_1(const vector<int> &arr, int k) {
         }
     }
 }
+
+size_t Problem_12_1_Binary_Search(const vector<int> &arr, int k) {
+    size_t begin = 0, end = arr.size() - 1, mid = (begin + end) / 2;
+    size_t result = -1;
+
+    while (begin <= end) {
+        if (k < arr[mid]) {
+            end = mid - 1;
+        } else if (k > arr[mid]) {
+            begin = mid + 1;
+        } else if (k == arr[mid]) {
+            result = mid;
+            end = mid - 1;
+        }
+
+        mid = (begin + end) / 2;
+    }
+
+    return result;
+}
--- Searching/test.cpp
+++ Searching/test.cpp
@@ -5,8 +5,10 @@
 using namespace std;
 
 extern size_t Problem_12_1(const vector<int> &arr, int k);
+extern size_t Problem_12_1_Binary_Search(const vector<int> &arr, int k);
 
 TEST_CASE("SEARCH A SORTED ARRAY FOR FIRST OCCURRENCE OF K") {
     const vector<int> a1{-14, -10, 2, 108, 108, 243, 285, 285, 285, 401};
     REQUIRE(Problem_12_1(a1, 108) == 3);
+    REQUIRE(Problem_12_1_Binary_Search(a1, 108) == 3);
 }
