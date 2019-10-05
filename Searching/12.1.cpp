67c304f8548a116ff89288ee1ab516356273987c
zhaoxiang <xiang.zhao@ucdconnect.ie>
2019-10-05 16:22:26 +0100

    Searching - 12.1 Variant 2

--- Searching/12.1.cpp
+++ Searching/12.1.cpp
@@ -47,3 +47,21 @@ size_t Problem_12_1_Variant_1(const vector<int> &arr, int k) {
 
     return result;
 }
+
+size_t Problem_12_1_Variant_2(const vector<int> &arr) {
+    size_t begin = 0, end = arr.size() - 1, mid = begin + (end - begin) / 2;
+
+    while (begin <= end) {
+        if (arr[mid] <= arr[mid - 1] and arr[mid] <= arr[mid + 1]) {
+            return mid;
+        } else if (arr[mid] > arr[mid + 1]) {
+            begin = mid + 1;
+        } else if (arr[mid] < arr[mid + 1]) {
+            end = mid - 1;
+        }
+
+        mid = begin + (end - begin) / 2;
+    }
+
+    return mid;
+}
--- Searching/test.cpp
+++ Searching/test.cpp
@@ -7,6 +7,7 @@ using namespace std;
 extern size_t Problem_12_1(const vector<int> &arr, int k);
 extern size_t Problem_12_1_Binary_Search(const vector<int> &arr, int k);
 extern size_t Problem_12_1_Variant_1(const vector<int> &arr, int k);
+extern size_t Problem_12_1_Variant_2(const vector<int> &arr);
 
 TEST_CASE("SEARCH A SORTED ARRAY FOR FIRST OCCURRENCE OF K") {
     const vector<int> a1{-14, -10, 2, 108, 108, 243, 285, 285, 285, 401};
@@ -14,4 +15,9 @@ TEST_CASE("SEARCH A SORTED ARRAY FOR FIRST OCCURRENCE OF K") {
     REQUIRE(Problem_12_1_Binary_Search(a1, 108) == 3);
     REQUIRE(Problem_12_1_Binary_Search(a1, 108) == 3);
     REQUIRE(Problem_12_1_Variant_1(a1, 285) == 9);
+
+    const vector<int> a2{5, 4, 3, 2, 1, 2};
+    const vector<int> a3{5, 4, 3, 2, 1, 1, 2, 3, 4, 5};
+    REQUIRE(Problem_12_1_Variant_2(a2) == 4);
+    REQUIRE(Problem_12_1_Variant_2(a3) == 4);
 }
