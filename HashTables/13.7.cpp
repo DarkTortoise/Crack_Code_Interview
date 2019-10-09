c5a4016860915573e2dba787dd5892036466cc93
zhaoxiang <xiang.zhao@ucdconnect.ie>
2019-10-09 14:11:41 +0100

    Hash Tables - 13.7 Variant 1

--- HashTables/13.7.cpp
+++ HashTables/13.7.cpp
@@ -1,10 +1,11 @@
 #include <unordered_set>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
-pair<size_t, size_t> Problem_13_7(const vector<string> &arr, unordered_set<string> &queries) {
-    unordered_set<string> covered{};
+pair<int, int> Problem_13_7(const vector<string> &arr, unordered_set<string> &queries) {
+    unordered_map<string, int> covered{};
     int lastBegin = 0, lastEnd = 0;
     int begin = 0, end = -1;
 
@@ -15,16 +16,37 @@ pair<size_t, size_t> Problem_13_7(const vector<string> &arr, unordered_set<strin
             }
             string value = arr[++end];
             if (queries.find(value) != queries.end()) {
-                covered.insert(value);
+                if (covered.find(value) == covered.end()) {
+                    covered.insert_or_assign(value, 1);
+                } else {
+                    covered.insert_or_assign(value, covered[value] + 1);
+                }
             }
         }
 
         lastBegin = begin, lastEnd = end;
 
         string value = arr[begin];
-        covered.erase(value);
+
+        if (queries.find(value) != queries.end()) {
+            covered.insert_or_assign(value, covered[value] - 1);
+            if (covered[value] <= 0) {
+                covered.erase(value);
+            }
+        }
+
         begin++;
     }
 
     return make_pair(lastBegin, lastEnd);
+}
+
+pair<int, int> Problem_13_7_Variant_1(const vector<string> &arr) {
+    unordered_set<string> e{};
+
+    for (auto &value: arr) {
+        e.insert(value);
+    }
+
+    return Problem_13_7(arr, e);
 }
--- HashTables/test.cpp
+++ HashTables/test.cpp
@@ -8,7 +8,8 @@ using namespace std;
 extern bool Problem_13_1(const string &str);
 extern bool Problem_13_2(const string &letter, const string &mag);
 extern int Problem_13_6(const vector<string> &arr);
-extern pair<size_t, size_t> Problem_13_7(const vector<string> &arr, unordered_set<string> &queries);
+extern pair<int, int> Problem_13_7(const vector<string> &arr, unordered_set<string> &queries);
+extern pair<int, int> Problem_13_7_Variant_1(const vector<string> &arr);
 
 TEST_CASE("test for palindrimic permutations") {
     REQUIRE(Problem_13_1("edified"));
@@ -29,9 +30,13 @@ TEST_CASE("FIND THE SMALLEST SUBARRAY COVERING ALL VALUES") {
                             "dog", "banana", "apple", "cat", "dog"};
     unordered_set<string> s1{"banana", "cat"};
     unordered_set<string> s2{"cat", "dog"};
-    pair<size_t, size_t> r1 = make_pair(8, 10);
-    pair<size_t, size_t> r2 = make_pair(10, 11);
+    pair<int, int> r1 = make_pair(8, 10);
+    pair<int, int> r2 = make_pair(10, 11);
+    pair<int, int> r3 = make_pair(8, 11);
 
     REQUIRE(Problem_13_7(a1, s1) == r1);
     REQUIRE(Problem_13_7(a1, s2) == r2);
+
+    auto r = Problem_13_7_Variant_1(a1);
+    REQUIRE(Problem_13_7_Variant_1(a1) == r3);
 }
