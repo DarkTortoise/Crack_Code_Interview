8a01a3d31189bb559f16e84af25e9c87a3eda256
zhaoxiang <xiang.zhao@ucdconnect.ie>
2019-10-14 21:05:02 +0100

    Binary Search Trees - 15.6

--- BinarySearchTrees/15.6.h
+++ BinarySearchTrees/15.6.h
@@ -12,29 +12,37 @@ template<class T>
 vector<T> Problem_15_6(const vector<T> &v1, const vector<T> &v2, const vector<T> &v3) {
     auto ptr1 = v1.begin(), ptr2 = v2.begin(), ptr3 = v3.begin();
     T minInterval = numeric_limits<T>::max();
-    multiset<T> s{};
     auto minPtr1 = v1.begin(), minPtr2 = v2.begin(), minPtr3 = v3.begin();
 
-    while (ptr1 != v1.end() || ptr2 != v2.end() || ptr3 != v3.end()) {
-        if ((*s.rbegin() - *s.begin()) < minInterval) {
+    while (ptr1 != v1.end() && ptr2 != v2.end() && ptr3 != v3.end()) {
+        auto minPtr = min({ptr1, ptr2, ptr3},
+                          [](auto &leftValue,
+                             auto &rightValue) {
+                              return *leftValue < *rightValue;
+                          });
+
+        auto maxPtr = max({ptr1, ptr2, ptr3},
+                          [](auto &leftValue,
+                             auto &rightValue) {
+                              return *leftValue < *rightValue;
+                          });
+
+        if (*maxPtr - *minPtr < minInterval) {
             minPtr1 = ptr1;
             minPtr2 = ptr2;
             minPtr3 = ptr3;
         }
 
-        if (*s.begin() == *ptr1) {
-            s.erase(*ptr1);
-            s.emplace(*(++ptr1));
-        } else if (*s.begin() == *ptr2) {
-            s.erase(*ptr2);
-            s.emplace(*(++ptr2));
-        } else if (*s.begin() == *ptr3) {
-            s.erase(*ptr3);
-            s.emplace(*(++ptr3));
+        if (minPtr == ptr1) {
+            ptr1++;
+        } else if (minPtr == ptr2) {
+            ptr2++;
+        } else {
+            ptr3++;
         }
     }
 
-    return vector<T>{*ptr1, *ptr2, *ptr3};
+    return vector<T>{*minPtr1, *minPtr2, *minPtr3};
 }
 
 #endif //BINARYSEARCHTREES_15_6_H
