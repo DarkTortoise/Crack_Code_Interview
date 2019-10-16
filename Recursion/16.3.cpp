6a9928ef10ad463fc37e3b3812c2c163fac3e7ba
zhaoxiang <xiang.zhao@ucdconnect.ie>
2019-10-16 11:09:12 +0100

    Recursion - 16.3

---
+++ Recursion/16.3.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void GetPerm(vector<int> &arr, size_t index, vector<vector<int>> &result) {
+    if (index >= arr.size()) {
+        result.push_back(arr);
+        return;
+    }
+
+    for (size_t i = index; i < arr.size(); i++) {
+        swap(*(arr.begin() + index), *(arr.begin() + i));
+        GetPerm(arr, index + 1, result);
+        swap(*(arr.begin() + index), *(arr.begin() + i));
+    }
+}
+
+void Problem_16_3(const vector<int> &arr, vector<vector<int>> &result) {
+    int index = 0;
+    vector<int> perm(arr);
+    GetPerm(perm, index, result);
+}
--- Recursion/test.cpp
+++ Recursion/test.cpp
@@ -1,1 +1,13 @@
 #include <catch2/catch.hpp>
+#include <vector>
+
+using namespace std;
+
+extern void Problem_16_3(const vector<int> &arr, vector<vector<int>> &result);
+
+TEST_CASE("16.3") {
+    const vector<int> a1{2, 3, 5, 7};
+    vector<vector<int>> result{};
+    Problem_16_3(a1, result);
+    REQUIRE(result.size() == 24);
+}
