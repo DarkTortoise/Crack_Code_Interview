35297ce42ade6849ba8a8fb3c99a61fe50d91d85
zhaoxiang <xiang.zhao@ucdconnect.ie>
2019-10-16 18:56:06 +0100

    Recursion - 16.3 Variant

--- Recursion/16.3.cpp
+++ Recursion/16.3.cpp
@@ -21,3 +21,25 @@ void Problem_16_3(const vector<int> &arr, vector<vector<int>> &result) {
     vector<int> perm(arr);
     GetPerm(perm, index, result);
 }
+
+void GetPermVariant(vector<int> &arr, size_t index, vector<vector<int>> &result) {
+    if (index >= arr.size()) {
+        result.push_back(arr);
+        return;
+    }
+
+    for (size_t i = index; i < arr.size(); i++) {
+        if (arr[*(arr.begin() + index)] != arr[*(arr.begin() + i)])
+            continue;
+
+        swap(*(arr.begin() + index), *(arr.begin() + i));
+        GetPerm(arr, index + 1, result);
+        swap(*(arr.begin() + index), *(arr.begin() + i));
+    }
+}
+
+void Problem_16_3_Variant(const vector<int> &arr, vector<vector<int>> &result) {
+    int index = 0;
+    vector<int> perm(arr);
+    GetPermVariant(perm, index, result);
+}
--- Recursion/test.cpp
+++ Recursion/test.cpp
@@ -4,10 +4,16 @@
 using namespace std;
 
 extern void Problem_16_3(const vector<int> &arr, vector<vector<int>> &result);
+extern void Problem_16_3_Variant(const vector<int> &arr, vector<vector<int>> &result);
 
 TEST_CASE("16.3") {
     const vector<int> a1{2, 3, 5, 7};
     vector<vector<int>> result{};
     Problem_16_3(a1, result);
     REQUIRE(result.size() == 24);
+
+    const vector<int> a2{2, 2, 3, 0};
+    vector<vector<int>> r2{};
+    Problem_16_3_Variant(a2, r2);
+    REQUIRE(r2.size() == 12);
 }
