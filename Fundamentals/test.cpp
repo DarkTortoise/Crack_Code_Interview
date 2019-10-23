d8dfaa8b58fd3161c0992416060e6412519db27b
zhaoxiang <xiang.zhao@ucdconnect.ie>
2019-10-24 00:01:09 +0100

    Fundamentals - Selection Sort

---
+++ Fundamentals/test.cpp
@@ -0,0 +1,13 @@
+#include <catch2/catch.hpp>
+#include <string>
+
+using namespace std;
+
+extern void selection_sort(vector<int> &arr);
+
+TEST_CASE("Selection sort") {
+    vector<int> a1{5, 4, 3, 2, 1};
+    vector<int> r1{1, 2, 3, 4, 5};
+    selection_sort(a1);
+    REQUIRE(a1 == r1);
+}
