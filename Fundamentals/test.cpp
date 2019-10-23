c3e41d4e7cb87e0910d45e44fe54ff5c93ed5029
zhaoxiang <xiang.zhao@ucdconnect.ie>
2019-10-24 00:10:42 +0100

    Fundamentals - Bubble Sort

--- Fundamentals/test.cpp
+++ Fundamentals/test.cpp
@@ -4,10 +4,18 @@
 using namespace std;
 
 extern void selection_sort(vector<int> &arr);
+extern void bubble_sort(vector<int> &arr);
 
 TEST_CASE("Selection sort") {
     vector<int> a1{5, 4, 3, 2, 1};
     vector<int> r1{1, 2, 3, 4, 5};
     selection_sort(a1);
+    REQUIRE(a1 == r1);
+}
+
+TEST_CASE("Bubble sort") {
+    vector<int> a1{5, 4, 3, 2, 1};
+    vector<int> r1{1, 2, 3, 4, 5};
+    bubble_sort(a1);
     REQUIRE(a1 == r1);
 }
